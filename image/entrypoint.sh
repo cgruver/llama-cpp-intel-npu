74c4b56f6d099aedc41e33ef568e6abd14188492
cgruver <cgruver@clg.lab>
2025-01-24 19:47:41 +0000

    update with ubi9

---
+++ image/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Create Home directory
+if [ ! -d "${HOME}" ]
+then
+  mkdir -p "${HOME}"
+fi
+
+# Create User ID
+if ! whoami &> /dev/null
+then
+  if [ -w /etc/passwd ]
+  then
+    echo "${USER_NAME:-llama-user}:x:$(id -u):105:${USER_NAME:-llama-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-llama-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
